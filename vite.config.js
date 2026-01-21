8b85ad035f986c1c6fdfc4961cd03542e6316195
Srishti Verma <srishtiverma358@gmail.com>
2026-01-21 22:14:43 +0530

    aading progress bar

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        cardProgress: "public/card-progress.html",
+        settings: "public/settings.html",
+        confirmRestart: "public/confirm-restart.html",
+        autoTrack: "public/auto-track-lists.html",
+      },
+    },
+  },
+  publicDir: "public",
+});
